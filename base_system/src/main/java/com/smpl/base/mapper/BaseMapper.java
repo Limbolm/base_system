2622fcc2ff44810971b40c15209a06c7d3450276
chenyepei <chenyepei@shsnc.com>
2017-09-04 16:48:14 +0800

    重写BaseMapper

--- base_system/src/main/java/com/smpl/base/mapper/BaseMapper.java
+++ base_system/src/main/java/com/smpl/base/mapper/BaseMapper.java
@@ -8,6 +8,7 @@ import java.util.Objects;
 
 /**
  * 公共mpper类
+ *  做为通用的dao 提供最基础的RCUD
  */
 public interface BaseMapper {
 
@@ -61,6 +62,19 @@ public interface BaseMapper {
      * @throws Exception
      */
     List<DataMap> selectListqueryPage(DataMap map)throws Exception;
+
+    /**
+     *
+     *此为基础的dao层 只允许baseSevce 调用 其他的 业务sevce 全部继承 baseSevce
+     * 1.findByPage 分页查询
+     * 2.findById 根据id 查询数据
+     * 3.findByList 不分页查询
+     * 4.findByAttribute  根据条件查询 可有多个条件
+     * 5.insert 新增
+     * 6.deleteById 删除 根据id 单个传入id  多个传入 ids 以','分隔
+     * 7.deleteByAttribute 根绝条件删除
+     * 8.update 更新
+     */
 
 
 
--- base_system/src/main/java/com/smpl/base/mapper/BaseMapperImpl.java
+++ base_system/src/main/java/com/smpl/base/mapper/BaseMapperImpl.java
@@ -16,80 +16,79 @@ import java.util.List;
  */
 
 @Service(value = "baseMapper")
-public class BaseMapperImpl extends SqlSessionDaoSupport implements BaseMapper{
+public class BaseMapperImpl extends SqlSessionDaoSupport implements BaseMapper {
 
-        //使用sqlSessionFactory
-        @Autowired
-        private  SqlSessionFactory sqlSessionFactory;
+    //使用sqlSessionFactory
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
 
     @Autowired
-    public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory)
-    {
+    public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
         super.setSqlSessionFactory(sqlSessionFactory);
     }
 
 
-        @Override
-        public DataMap selectById(DataMap map)throws Exception {
-            map=builSqlData("Base-mapper.selectById",map);
-            getSqlSession().selectOne(map.getStr("MapperId"),map);
-            return map;
-        }
-
-        @Override
-        public List<DataMap> selectList(DataMap map)throws Exception {
-            map=builSqlData("Base-mapper.selectByAttribute",map);
-            return getSqlSession().selectList(map.getStr("MapperId"),map);
-        }
-
     @Override
-    public List<DataMap> selectByAttribute(DataMap map) throws Exception {
-        return null;
+    public DataMap selectById(DataMap map) throws Exception {
+        map = builSqlData("Base-mapper.selectById", map);
+        getSqlSession().selectOne(map.getStr("MapperId"), map);
+        return map;
     }
 
     @Override
-        public void delete(DataMap map) throws Exception{
-            map=builSqlData("Base-mapper.deleteByIds",map);
-            getSqlSession().delete(map.getStr("MapperId"),map);
-        }
+    public List<DataMap> selectList(DataMap map) throws Exception {
+        map = builSqlData("Base-mapper.selectByAttribute", map);
+        return getSqlSession().selectList(map.getStr("MapperId"), map);
+    }
 
-        @Override
-        public Integer update(DataMap map)throws Exception {
-            map=builSqlData("Base-mapper.updateByid",map);
-            return getSqlSession().update(map.getStr("MapperId"),map);
-        }
+    @Override
+    public List<DataMap> selectByAttribute(DataMap map) throws Exception {
+        map = builSqlData("Base-mapper.selectByAttribute", map);
+        return getSqlSession().selectList(map.getStr("MapperId"), map);
+    }
 
-        @Override
-        public Integer insert(DataMap map) throws Exception{
-            map=builSqlData("Base-mapper.addEntity",map);
-            return getSqlSession().insert(map.getStr("MapperId"),map);
-        }
+    @Override
+    public void delete(DataMap map) throws Exception {
+        map = builSqlData("Base-mapper.deleteByIds", map);
+        getSqlSession().delete(map.getStr("MapperId"), map);
+    }
+
+    @Override
+    public Integer update(DataMap map) throws Exception {
+        map = builSqlData("Base-mapper.updateByid", map);
+        return getSqlSession().update(map.getStr("MapperId"), map);
+    }
+
+    @Override
+    public Integer insert(DataMap map) throws Exception {
+        map = builSqlData("Base-mapper.addEntity", map);
+        return getSqlSession().insert(map.getStr("MapperId"), map);
+    }
 
     @Override
     public List<DataMap> selectListqueryPage(DataMap map) throws Exception {
-        return null;
+        map = builSqlData("Base-mapper.QueryBypage", map);
+        return getSqlSession().selectList(map.getStr("MapperId"), map);
     }
 
     /**
      * 构建sql方法
      */
 
-        private DataMap builSqlData(String mapperStr,DataMap map)throws Exception{
+    private DataMap builSqlData(String mapperStr, DataMap map) throws Exception {
 
-            //判断 map中是否 传入MapperId  如无则使用 mapperStr
-            if (StringUtils.isEmpty(map.getStr(map.getMapperId()))){
-                map.put("MapperId",mapperStr);
-            }else {
-                map.put("MapperId",map.getMapperId());
-            }
-
-            if (StringUtils.isEmpty(map.getStr("tableName"))){
-                throw new BusinessException("获取不到表名，查询失败！");
-            }
-            return map;
+        //判断 map中是否 传入MapperId  如无则使用 mapperStr
+        if (!map.containsKey("MapperId")) {
+            map.put("MapperId", mapperStr);
+        } else {
+            map.put("MapperId", map.getMapperId());
         }
 
-
+        if (map.containsKey("tableName")) {
+            throw new BusinessException("获取不到表名，查询失败！");
+        }
+        return map;
+    }
 
 
 }
